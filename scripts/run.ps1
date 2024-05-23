6069962311ab1dff9a8e71a38501d24078d07ffd
Marek <xsykoram3@stuba.sk>
2024-05-23 23:39:46 +0200

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,13 +10,26 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:API_ENVIRONMENT="Development"
 $env:API_PORT="8080"
+$env:API_MONGODB_USERNAME="root"
+$env:API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/sprava-krvi-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/sprava-krvi-api-service
+            mongo down
+        }
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "mongo" {
+        mongo up
     }
     default {
         throw "Unknown command: $command"
